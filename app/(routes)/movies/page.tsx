2f8845cca947176dc9fde870bc3f2b824a4d1a4f
philipnyakwaka1 <philipnyakwaka24@gmail.com>
2024-02-17 12:19:01 +0300

    changed landing page

--- app/(routes)/movies/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Movies = () => {
-  return (
-    <div>Movies page</div>
-  )
-}
-
-export default Movies
--- lib/get-genre.tsx
+++
@@ -1,9 +0,0 @@
-import { GenreMap } from "@/constants/genres";
-
-export function getGenreNames(genres: number[]) {
-    let genreNames: string[] = [];
-    genres.forEach((genre) => {
-      genreNames.push(GenreMap[genre]);
-    });
-    return genreNames;
-  }
